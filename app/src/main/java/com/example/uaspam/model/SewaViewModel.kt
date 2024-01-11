ec8327e0c5b38014593208b011e1c97d7d3c2c01
raniagisela <raniagslp@gmail.com>
2024-01-11 12:58:50 +0700

    menambahkan variabel dan data class SewaUiState

--- app/src/main/java/com/example/uaspam/model/SewaViewModel.kt
+++ app/src/main/java/com/example/uaspam/model/SewaViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.uaspam.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.repositori.RepositoriSewa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 class SewaViewModel(private val repositoriSewa: RepositoriSewa): ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
 
-    
+    val sewaUiState : StateFlow<SewaUiState> = repositoriSewa.getAllSewaStream().filterNotNull()
+        .map { SewaUiState(listSewa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = SewaUiState()
+        )
+
+    data class SewaUiState(
+        val listSewa: List<Sewa> = listOf()
+    )
+}
+
+
+
