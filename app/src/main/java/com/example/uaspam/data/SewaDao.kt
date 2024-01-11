bd0989ca2ad9ef01a85b2e4fe2fe5940c096245c
raniagisela <raniagslp@gmail.com>
2024-01-11 12:03:58 +0700

    menambahkan interface SewaDao

---
+++ app/src/main/java/com/example/uaspam/data/SewaDao.kt
@@ -0,0 +1,27 @@
+package com.example.uaspam.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SewaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Sewa)
+
+    @Update
+    suspend fun update(siswa: Sewa)
+
+    @Delete
+    suspend fun delete(siswa: Sewa)
+
+    @Query("SELECT * from tblSewa WHERE id = :id")
+    fun getSewa(id: Int): Flow<Sewa>
+
+    @Query("SELECT * from tblSewa ORDER BY nama ASC")
+    fun getAllSewa(): Flow<List<Sewa>>
+}
